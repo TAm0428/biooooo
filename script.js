dd7abc6be033d0be9ad918aa27c461205ca2948f
TAm0428 <cts88347003@gmail.com>
2026-06-22 11:14:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleInfo(id){
+
+    let section = document.getElementById(id);
+
+    if(section.style.display === "block"){
+        section.style.display = "none";
+    }
+    else{
+        section.style.display = "block";
+    }
+
+}
+
